d9938d183c6ccb304daa68daa3ff093e4d0ec340
Fatlindd <fatlind.thaqi98@gmail.com>
2021-06-15 19:09:14 +0200

    Largimi Action Bar nga hyrja ne Aplikacion!

--- app/src/main/java/fiek/unipr/online_rent_a_car/MainActivity.java
+++ app/src/main/java/fiek/unipr/online_rent_a_car/MainActivity.java
@@ -2,12 +2,20 @@ package fiek.unipr.online_rent_a_car;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        //Largimi i ActionBar
+        if(getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }
+
     }
 }
