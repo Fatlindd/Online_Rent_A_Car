ec49f447d7ba13683697d49516938836ddc67536
Fatlindd <fatlind.thaqi98@gmail.com>
2021-06-18 20:58:28 +0200

    setOnClickListener metoda tek login,per te kalu nga LogIn form ne RegisterForm nese nuk ka llogari aktive!

--- app/src/main/java/fiek/unipr/online_rent_a_car/Login.java
+++ app/src/main/java/fiek/unipr/online_rent_a_car/Login.java
@@ -2,14 +2,29 @@ package fiek.unipr.online_rent_a_car;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 public class Login extends AppCompatActivity {
 
+    Button button_register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        button_register = (Button)findViewById(R.id.register) ;
+
+    button_register.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent registerIntent=new Intent(Login.this,register_activity.class);
+            startActivity(registerIntent);
+        }
+    });
 
     }
 }
