887509ab20db49403616948b1f7370a456030f66
Fatlindd <fatlind.thaqi98@gmail.com>
2021-06-21 02:42:24 +0200

    Krijimi i Menu me disa item!

--- app/src/main/java/fiek/unipr/online_rent_a_car/HomeActivity.java
+++ app/src/main/java/fiek/unipr/online_rent_a_car/HomeActivity.java
@@ -1,8 +1,15 @@
 package fiek.unipr.online_rent_a_car;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +17,21 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.home_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+       if(item.getItemId() == R.id.logut)
+           Toast.makeText(HomeActivity.this,"Butoni LogOut u klikua",Toast.LENGTH_LONG).show();
+           return super.onOptionsItemSelected(item);
     }
 }
